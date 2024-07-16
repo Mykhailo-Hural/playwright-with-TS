4dc0e5b25774a97aa9e31f5ad41d65f122727e8a
Mykhailo <mykhailo_hural@epam.com>
2024-07-16 15:19:14 +0300

    workshop_1

---
+++ tests/workshop_1/workshop_1.spec.ts
@@ -0,0 +1,13 @@
+import { test } from 'playwright/test';
+
+test('Basic Navigation', async ({ page }) => {
+    await page.goto('https://gitlab.com/');
+    await page.waitForTimeout(3000);
+    await page.reload();
+});
+
+test.only('Interaction with the Web Element on Gitlab', async ({ page }) => {
+    await page.goto('https://gitlab.com/');
+    // Corrected line below
+    await page.locator('.be-nav-navigation-bottom >> text="Get free trial"').click();
+});
