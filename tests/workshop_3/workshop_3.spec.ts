d849fd5b988ebfc7ef0aef16422d90e53140e6ae
Mykhailo <mykhailo_hural@epam.com>
2024-07-18 11:45:43 +0300

    fixed link to the page

--- tests/workshop_3/workshop_3.spec.ts
+++ tests/workshop_3/workshop_3.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Advance Interaction', async ({ page }) => {
-  await page.goto('http://127.0.0.1:5500/tests/workshop_3/index.html');
+  await page.goto('file://' + __dirname + '/index.html');
   // Hover over the element with the text "Hover me"
   await page.hover('#hover-me');
   // Wait for the text "Text Changed!" to be visible on the page with an explicit timeout
@@ -19,14 +19,14 @@ test('Advance Interaction', async ({ page }) => {
 });
 
 test('Drag and Drop', async ({ page }) => {
-  await page.goto('http://127.0.0.1:5500/tests/workshop_3/index.html');
+  await page.goto('file://' + __dirname + '/index.html');
   await page.dragAndDrop('.drag-source', '.drop-target');
   // Corrected assertion to properly use Playwright's expect API
   await expect(page.locator('.drop-target')).toContainText('Success');
 });
 
 test('Handling iframe', async ({ page }) => {
-  await page.goto('http://127.0.0.1:5500/tests/workshop_3/index.html');
+  await page.goto('file://' + __dirname + '/index.html');
   const iframeElement = await page.frame({ name: 'iframeName' });
   const inputSelector = '#iframe-input';
 
